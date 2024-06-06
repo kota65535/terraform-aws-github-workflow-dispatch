034756a5c58afc2c323ade2bfc08dc26abd0882d
Tomohiko Ozawa <kota65535@gmail.com>
2024-06-06 18:43:19 +0900

    Enable jinja2 template (#22)

--- lambda/src/function.py
+++ lambda/src/function.py
@@ -1,10 +1,11 @@
 import json
 import logging
-from typing import TypedDict
+from typing import TypedDict, NotRequired
 
 import common
 from common import GitHubClient
 from config import Config
+from jinja2 import Template
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,19 +17,40 @@ class WorkflowDispatchRequest(TypedDict):
     owner: str
     repo: str
     workflow: str
-    ref: str
-    inputs: dict
+    ref: NotRequired[str]
+    inputs: NotRequired[dict]
 
 
 # Lambda function entrypoint
 def lambda_handler(event: WorkflowDispatchRequest, context):
     logger.info(f"Input: {json.dumps(event)}")
 
+    event = render(event)
+
+    logger.info(f"Rendered: {json.dumps(event)}")
+
     dispatch_workflow(event)
 
 
-def dispatch_workflow(req: WorkflowDispatchRequest):
+def render(req: WorkflowDispatchRequest) -> WorkflowDispatchRequest:
+    out = {
+        "owner": Template(req["owner"]).render(),
+        "repo": Template(req["repo"]).render(),
+        "workflow": Template(req["workflow"]).render(),
+    }
+    if "ref" in req:
+        out["ref"] = Template(req["ref"]).render()
+ 
+    if "inputs" in req:
+        inputs = {}
+        for k, v in req["inputs"].items():
+            inputs[k] = Template(v).render()
+        out["inputs"] = inputs
 
+    return out
+
+
+def dispatch_workflow(req: WorkflowDispatchRequest):
     client = GitHubClient(config.github_token)
 
     if "ref" in req:
